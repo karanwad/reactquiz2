facdaafd3853eccee07169944ae553d9fac8b441
Karan Wadhwani <kwadhwani@xpanxion.com>
2023-05-30 10:34:37 -0400

    react 2 quiz question 11

--- src/NumberBuilder.jsx
+++ src/NumberBuilder.jsx
@@ -1,13 +1,24 @@
-import React from "react";
-
+import React, {useState} from "react";
 export const NumberBuilder = () => {
+    let [counter, setCounter] = useState(0);
+    let [stringCounter, setStringCounter] = useState('');
+
+    const handleAddNum = () => {
+        setCounter(counter += 1);
+        setStringCounter(stringCounter + " " + counter);
+    }
+
+    const handleReset = () => {
+        setCounter(0);
+        setStringCounter('');
+    }
 
     return (
         <>
-            <button>Add Number</button>
-            <button>Reset</button>
+            <button onClick={()=>handleAddNum()}>Add Number</button>
+            <button onClick={()=>handleReset()}>Reset</button>
             <br></br>
-            Numbers: 
+            Numbers: {stringCounter} 
         </>
     )
 }
